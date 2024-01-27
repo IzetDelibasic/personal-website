0a27e6a08f94579fdacb431d005126aa65122d1d
Izet Delibasic <izetdelibasic55@gmail.com>
2024-01-27 23:28:43 +0100

    Updating experience section

--- app/components/Experience/Experience.tsx
+++ app/components/Experience/Experience.tsx
@@ -3,12 +3,9 @@ import React from 'react'
 const Experience = () => {
   return (
     <div id="experience" className="my-[5rem] lg:mt-[10rem] space-y-8 lg:space-y-10 relative">
-        <div className="text-3xl lg:text-5xl font-light flex gap-2 flex-wrap justify-center text-center items-center card border-gray-200 border-4 p-10 lg:w-[75%] xl:w-[25%] mx-auto font-subtitle">
-          <span className="font-medium">Experience</span>
-        </div>
-        <div className='lg:border-b-2 lg:border-white pt-[5rem] lg:w-[60%] mx-auto'>
-          <div className='text-white text-center font-subtitle text-[3rem]'>
-            Education and courses
+        <div className='lg:border-b-2 lg:border-white pt-[5rem] lg:w-[80%] mx-auto'>
+          <div className="text-3xl lg:text-5xl font-light flex gap-2 flex-wrap justify-center text-center items-center card border-gray-200 border-4 p-10 lg:w-[85%] xl:w-[45%] mx-auto font-subtitle my-[1rem]">
+            <span className="font-medium">Education and courses</span>
           </div>
           <div className='w-[95%] mx-auto text-center font-subtitle flex lg:flex-row flex-col lg:justify-between justify-center items-center'>
             <div className='my-[1rem] lg:w-full w-[90%] mx-auto lg:border-r-0 border-b-2 lg:border-b-0 border-gray-400 lg:px-[1rem] py-[1rem]'>
@@ -20,8 +17,8 @@ const Experience = () => {
           </div>
         </div>
         <div className='border-b-2 border-white pt-[5rem]'>
-          <div className='text-white text-center font-subtitle text-[3rem]'>
-            Competitive experience
+          <div className="text-3xl lg:text-5xl font-light flex gap-2 flex-wrap justify-center text-center items-center card border-gray-200 border-4 p-10 lg:w-[85%] xl:w-[45%] mx-auto font-subtitle my-[1rem]">
+            <span className="font-medium">Competitive experience</span>
           </div>
           <div className='w-[95%] mx-auto text-center font-subtitle flex lg:flex-row flex-col lg:justify-between justify-center items-center'>
             <div className='my-[1rem] lg:w-[60%] w-[90%] mx-auto lg:border-r-2 border-b-2 lg:border-b-0 border-gray-400 lg:px-[1rem] py-[1rem]'>
