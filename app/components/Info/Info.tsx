a91ad9cbfd277a543059d65c6d8098ec2a2401d7
Izet Delibasic <izetdelibasic55@gmail.com>
2024-03-13 02:15:41 +0100

    Moving projects data in constant and updating responsive and desktop style

--- app/components/Info/Info.tsx
+++ app/components/Info/Info.tsx
@@ -5,14 +5,14 @@ import booksAnimation from '@/animations/book.json';
 
 export default function Info() {
   return (
-    <div className="text-2xl md:text-3xl xl:text-5xl 2xl:text-6xl font-light w-4/6 mx-auto text-center leading-5 lg:leading-relaxed flex flex-wrap relative z-50 gap-4 items-center justify-center font-about">
+    <div className="text-[1.5rem] sm:text-[2rem] lg:text-[2.2rem] xl:text-[2.7rem] font-light lg:w-[60%] w-[80%] mx-auto text-center flex flex-wrap relative z-50 gap-[1rem] items-center justify-center font-montserrat">
       <span className="text-gray-500"></span>
-      <span className="font-bold"><span className='text-blue-400'>Frontend</span> Developer</span>
+      <span className="font-semibold"><span className='text-blue-400'>Frontend</span> Developer</span>
       <span className="text-gray-500">in the learning process</span>
       <span className="text-gray-500">and a</span>
-      <span className="font-bold">Software Engineering</span>
+      <span className="font-semibold">Software Engineering</span>
       <span className="text-gray-500">Student</span>
-      <Lottie className="w-10 md:w-12 lg:w-24" animationData={booksAnimation} loop={true} />
+      <Lottie className="w-[2.5rem] md:w-[3rem] lg:w-[6rem]" animationData={booksAnimation} loop={true} />
     </div>
   );
 }
--- app/components/Projects/Projects.tsx
+++ app/components/Projects/Projects.tsx
@@ -1,122 +1,55 @@
-"use client";
-// -React-
 import React from "react";
-// -Next-
 import Image from "next/image";
 import dynamic from "next/dynamic";
-// -Button component-
+import { projectsConstant } from "@/utils/constants/projectConstant";
+
 const Button = dynamic(() => import("@/app/components/Button/Button"));
 
 const Projects = () => {
   return (
     <div className="">
       <div id="myprojects" className="mt-[5rem] lg:mt-[10rem] space-y-8 lg:space-y-10 relative">
-        <div className="text-3xl lg:text-5xl font-light flex gap-2 flex-wrap justify-center text-center items-center card border-gray-200 border-4 p-10 lg:w-[75%] xl:w-[25%] mx-auto font-subtitle">
+        <div className="text-[2rem] lg:text-[2.5rem] font-light flex gap-2 flex-wrap justify-center text-center items-center card border-gray-200 border-4 p-10 w-[90%] sm:w-[60%] md:w-[50%] xl:w-[25%] mx-auto font-subtitle">
           <span className="text-gray-500">Selected</span>
           <span className="font-medium">Works</span>
         </div>
 
-        <div className="space-y-20 lg:space-y-32 w-5/6 mx-auto">
-          {/* WORK 1 */}
-          <div className="flex w-full justify-center mt-[5rem]">  
-            <div className="grid grid-cols-1 lg:grid-cols-2 gap-[5rem] w-full lg:w-5/6 border-gray-500 border-b lg:border-b-0 pb-12 lg:pb-0">
-              <div className="col-span-1 w-full">
-                <Image
-                  src="/images/tasty-treasure.png"
-                  className="rounded-[2rem] lg:rounded-tl-none lg:rounded-br-none lg:hover:rounded-br-[2rem] lg:hover:rounded-tl-[2rem] transition-all duration-300 shadow-lg mx-auto"
-                  alt="work"
-                  width={600}
-                  height={600}
-                />
-              </div>
-              <div className="col-span-1 w-full h-full flex flex-col justify-center items-center space-y-8 font-about text-center">
-                <h1 className="uppercase font-medium">TASTY TREASURES</h1>
-                <p className="text-3xl font-light leading-relaxed">
-                  <span className="text-gray-500">This is my first solo project in </span>
-                  <span className="font-semibold">ReactJS</span>
-                  <span className="text-gray-500">, where I applied the knowledge I&apos;ve learned and built </span>
-                  <span className="font-semibold">a frontend application for recipe ideas.</span>
-                  <span className="text-gray-500"> I used Firebase as the database to allow users to save their profiles.</span>
-                </p>
-                <h6 className="font-light text-gray-400">Web Application Design</h6>
-                <Button
-                  className="relative bg-blue-400 text-black border-[1px] border-black font-medium lg:py-[1.5rem] py-[1rem] lg:px-[2rem] px-[4.5rem] md:px-[11.5rem] rounded-[3rem] group overflow-hidden z-[1] lg:ml-[20px] hover:border-opacity-0 hover:border-transparent"
-                  iconClassName=""
-                  title="View on Github"
-                  titleClassName="group-hover:text-white"
-                  href="https://github.com/IzetDelibasic/tasty-treasures"
-                >
-                  <div className="absolute inset-0 bg-neutral-100 w-full transform origin-right transition-transform duration-300 group-hover:scale-x-0 z-[-1]"></div>
-                </Button>
-              </div>
-            </div>
-          </div>
-          {/* WORK 2 */}
-          <div className="flex w-full justify-center mt-[5rem]">
-            <div className="grid grid-cols-1 lg:grid-cols-2 gap-[5rem] w-full lg:w-[80%] border-gray-500 border-b lg:border-b-0 pb-12 lg:pb-0 font-about text-center">
-              <div className="col-span-1 w-full h-full flex flex-col justify-center items-center space-y-8">
-                <h1 className="uppercase font-medium font-subtitle">Survival Quest</h1>
-                <p className="text-[1.75rem] font-light leading-relaxed">
-                  <span className="text-gray-500">A survival game being developed using </span>
-                  <span className="font-semibold">Unity and C# scripts</span>
-                  <span className="text-gray-500">, offering a high level of immersion and a wide range of features. The game is currently in the development phase, with much more to come soon.</span>
-                </p>
-                <h6 className="font-light text-gray-400 font-subtitle">Game Folder</h6>
-                <Button
-                  className="relative bg-blue-400 text-black border-[1px] border-black font-medium lg:py-[1.5rem] py-[1rem] lg:px-[2rem] px-[4.5rem] md:px-[11.5rem] rounded-[3rem] group overflow-hidden z-[1] lg:ml-[20px] hover:border-opacity-0 hover:border-transparent"
-                  iconClassName=""
-                  title="View on Github"
-                  titleClassName="group-hover:text-white"
-                  href="https://github.com/IzetDelibasic/survival-quest"
-                >
-                  <div className="absolute inset-0 bg-neutral-100 w-full transform origin-right transition-transform duration-300 group-hover:scale-x-0 z-[-1]"></div>
-                </Button>
-              </div>
-              <div className="col-span-1 w-full">
-                <Image
-                  src="/images/survival-quest.png"
-                  className="rounded-[2rem] lg:rounded-tr-none lg:rounded-bl-none lg:hover:rounded-bl-[2rem] lg:hover:rounded-tr-[2rem] transition-all duration-300 shadow-lg mx-auto"
-                  alt="work"
-                  width={600}
-                  height={600}
-                />
+        <div className="space-y-20 lg:space-y-32 w-[90%] md:w-[80%] mx-auto">
+          {projectsConstant.map((project, index) => (
+            <div
+              key={index}
+              className={`flex w-full items-center justify-center mt-[5rem] ${
+                index % 2 === 0 ? "" : "lg:flex-col"
+              }`}
+            >
+              <div className="grid grid-cols-1 lg:grid-cols-2 gap-[5rem] w-full lg:w-[90%] border-gray-500 border-b lg:border-b-0 pb-12 lg:pb-0">
+                <div className="col-span-1 w-full">
+                  <Image
+                    src={project.imageSrc}
+                    className="lg:w-full lg:h-full w-[25rem] rounded-[2rem] lg:rounded-tl-none lg:rounded-br-none lg:hover:rounded-br-[2rem] lg:hover:rounded-tl-[2rem] transition-all duration-300 shadow-lg mx-auto"
+                    alt="work"
+                    width={600}
+                    height={600}
+                  />
+                </div>
+                <div className="col-span-1 w-full h-full flex flex-col justify-center items-center space-y-8 font-about text-center">
+                  <h1 className="uppercase font-medium">{project.title}</h1>
+                  <p className="xl:text-[1.2rem] font-light leading-relaxed">
+                    {project.description}
+                  </p>
+                  <Button
+                    className="relative bg-blue-400 text-black border-[1px] border-black font-medium lg:py-[1.5rem] py-[1rem] lg:px-[3rem] px-[4.5rem] md:px-[11.5rem] rounded-[3rem] group overflow-hidden z-[1] lg:ml-[20px] hover:border-opacity-0 hover:border-transparent"
+                    iconClassName=""
+                    title={project.buttonTitle}
+                    titleClassName="group-hover:text-white"
+                    href={project.link}
+                  >
+                    <div className="absolute inset-0 bg-neutral-100 w-full transform origin-right transition-transform duration-300 group-hover:scale-x-0 z-[-1]"></div>
+                  </Button>
+                </div>
               </div>
             </div>
-          </div>
-          {/* WORK 1 */}
-          <div className="flex w-full justify-center mt-[5rem]">  
-            <div className="grid grid-cols-1 lg:grid-cols-2 gap-[5rem] w-full lg:w-5/6 border-gray-500 border-b lg:border-b-0 pb-12 lg:pb-0">
-              <div className="col-span-1 w-full">
-                <Image
-                  src="/images/sumathermImage.png"
-                  className="rounded-[2rem] lg:rounded-tl-none lg:rounded-br-none lg:hover:rounded-br-[2rem] lg:hover:rounded-tl-[2rem] transition-all duration-300 shadow-lg mx-auto"
-                  alt="work"
-                  width={600}
-                  height={600}
-                />
-              </div>
-              <div className="col-span-1 w-full h-full flex flex-col justify-center items-center space-y-8 font-about text-center">
-                <h1 className="uppercase font-medium">SUMATHERM GMBH</h1>
-                <p className="text-3xl font-light leading-relaxed">
-                  <span className="text-gray-500">This is my first</span>
-                  <span className="font-semibold"> production project</span>
-                  <span className="text-gray-500">, where I worked for BlueDuck IT Services & Solutions company and </span>
-                  <span className="font-semibold">built an app for Sumatherm Company using NextJS.</span>
-                  <span className="text-gray-500"> I used TypeScript and Tailwind CSS. I learned to work with reusable components, constants, and other features for faster page speed.</span>
-                </p>
-                <h6 className="font-light text-gray-400">Website Link</h6>
-                <Button
-                  className="relative bg-blue-400 text-black border-[1px] border-black font-medium lg:py-[1.5rem] py-[1rem] lg:px-[2rem] px-[4.5rem] md:px-[11.5rem] rounded-[3rem] group overflow-hidden z-[1] lg:ml-[20px] hover:border-opacity-0 hover:border-transparent"
-                  iconClassName=""
-                  title="Browse website"
-                  titleClassName="group-hover:text-white"
-                  href="https://sumatherm-project.vercel.app/"
-                >
-                  <div className="absolute inset-0 bg-neutral-100 w-full transform origin-right transition-transform duration-300 group-hover:scale-x-0 z-[-1]"></div>
-                </Button>
-              </div>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </div>
