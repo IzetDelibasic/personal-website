8d80c7ca8f8d01037247e25017f16a04e3f88c8a
Izet Delibasic <izetdelibasic55@gmail.com>
2024-03-28 00:23:58 +0100

    Adding new project. checking issue with deploying

--- utils/constants/projectConstant.ts
+++ utils/constants/projectConstant.ts
@@ -1,23 +1,33 @@
 export const projectsConstant = [
   {
     title: "Tasty Treasures",
-    description: "This is my first solo project in ReactJS, where I applied the knowledge I've learned and built a frontend application for recipe ideas. I used Firebase as the database to allow users to save their profiles.",
+    description:
+      "This is my first solo project in ReactJS, where I applied the knowledge I've learned and built a frontend application for recipe ideas. I used Firebase as the database to allow users to save their profiles.",
     imageSrc: "/images/tasty-treasure.png",
     link: "https://github.com/IzetDelibasic/tasty-treasures",
-    buttonTitle: "View repository"
+    buttonTitle: "View repository",
   },
   {
     title: "Survival Quest",
-    description: "A survival game being developed using Unity and C# scripts, offering a high level of immersion and a wide range of features. The game is currently in the development phase, with much more to come soon.",
+    description:
+      "A survival game being developed using Unity and C# scripts, offering a high level of immersion and a wide range of features. The game is currently in the development phase, with much more to come soon.",
     imageSrc: "/images/survival-quest.png",
     link: "https://github.com/IzetDelibasic/survival-quest",
-    buttonTitle: "View repository"
+    buttonTitle: "View repository",
   },
   {
     title: "SUMATHERM GMBH",
-    description: "This is my first production project, where I worked for BlueDuck IT Services & Solutions company and built an app for Sumatherm Company using NextJS. I used TypeScript and Tailwind CSS. I learned to work with reusable components, constants, and other features for faster page speed.",
+    description:
+      "This is my first production project, where I worked for BlueDuck IT Services & Solutions company and built an app for Sumatherm Company using NextJS. I used TypeScript and Tailwind CSS. I learned to work with reusable components, constants, and other features for faster page speed.",
     imageSrc: "/images/sumathermImage.png",
     link: "https://sumatherm-project.vercel.app/",
-    buttonTitle: "View website"
-  }
+    buttonTitle: "View website",
+  },
+  {
+    title: "HIREFY APP",
+    description: "Coming soon.",
+    imageSrc: "/images/sumathermImage.png",
+    link: "",
+    buttonTitle: "View application",
+  },
 ];
