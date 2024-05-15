da12e28d92b47a7cb1810d1b9c895ded1e56e75e
Izet Delibasic <izetdelibasic55@gmail.com>
2024-05-15 12:17:21 +0200

    Updating projects informations

--- utils/constants/projectConstant.ts
+++ utils/constants/projectConstant.ts
@@ -1,21 +1,5 @@
 export const projectsConstant = [
   {
-    title: "Tasty Treasures",
-    description:
-      "This is my first solo project in ReactJS, where I applied the knowledge I've learned and built a frontend application for recipe ideas. I used Firebase as the database to allow users to save their profiles.",
-    imageSrc: "/images/tasty-treasure.png",
-    link: "https://github.com/IzetDelibasic/tasty-treasures",
-    buttonTitle: "View repository",
-  },
-  {
-    title: "Survival Quest",
-    description:
-      "A survival game being developed using Unity and C# scripts, offering a high level of immersion and a wide range of features. The game is currently in the development phase, with much more to come soon.",
-    imageSrc: "/images/survival-quest.png",
-    link: "https://github.com/IzetDelibasic/survival-quest",
-    buttonTitle: "View repository",
-  },
-  {
     title: "SUMATHERM GMBH",
     description:
       "This is my first production project, where I worked for BlueDuck IT Services & Solutions company and built an app for Sumatherm Company using NextJS. I used TypeScript and Tailwind CSS. I learned to work with reusable components, constants, and other features for faster page speed.",
@@ -30,5 +14,21 @@ export const projectsConstant = [
     imageSrc: "/images/hirefyImage.png",
     link: "https://hirefy-app.onrender.com/",
     buttonTitle: "View application",
+  },
+  {
+    title: "Jumpin - Travel app",
+    description:
+      "We created a full-stack application for traveling. The front-end part I worked on was developed using ReactJs and TailwindCSS. This project will be presented at the FIT CC 2024 competition as part of the Innovation Challenge.",
+    imageSrc: "/images/jumpinImage.png",
+    link: "https://github.com/IzetDelibasic/jumpin-frontend",
+    buttonTitle: "View repository",
+  },
+  {
+    title: "Survival Quest",
+    description:
+      "A survival game being developed using Unity and C# scripts, offering a high level of immersion and a wide range of features. The game is currently in the development phase, with much more to come soon.",
+    imageSrc: "/images/survival-quest.png",
+    link: "https://github.com/IzetDelibasic/survival-quest",
+    buttonTitle: "View repository",
   },
 ];
